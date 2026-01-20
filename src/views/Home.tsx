69f534130e40fe4b4c2c920d8c6f96ea2dcdbcde
Reynaldo qs <reynaldoqs@gmail.com>
2026-01-20 10:17:07 -0400

    refactor: Standardize indentation and formatting in Home component for improved readability

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,16 @@
 import { HeroSection, OverviewCarousel, Statistics, Badge } from "@/components";
 
 export function Home() {
-	return (
-		<div className="w-full h-full relative p-1">
-			<Badge className="fixed top-4 right-4">this in progress</Badge>
-			<div className="absolute left-0 top-0 w-full lg:w-auto lg:left-18 lg:top-1/2 animate-in fade-in duration-1000">
-				<Statistics className="flex-row w-full justify-center mt-4 gap-4 lg:gap-8 lg:mt-0 lg:flex-col lg:-translate-y-1/2" />
-			</div>
-			<div className="absolute w-full flex justify-center lg:w-auto -bottom-[200px] lg:right-[-100px] lg:top-2/5 animate-in fade-in duration-1000">
-				<OverviewCarousel />
-			</div>
-			<HeroSection className="-mt-10 animate-in fade-in duration-1000" />
-		</div>
-	);
+  return (
+    <div className="w-full h-full relative p-1">
+      <Badge className="fixed top-4 right-4">This in progress</Badge>
+      <div className="absolute left-0 top-0 w-full lg:w-auto lg:left-18 lg:top-1/2 animate-in fade-in duration-1000">
+        <Statistics className="flex-row w-full justify-center mt-4 gap-4 lg:gap-8 lg:mt-0 lg:flex-col lg:-translate-y-1/2" />
+      </div>
+      <div className="absolute w-full flex justify-center lg:w-auto -bottom-[200px] lg:right-[-100px] lg:top-2/5 animate-in fade-in duration-1000">
+        <OverviewCarousel />
+      </div>
+      <HeroSection className="-mt-10 animate-in fade-in duration-1000" />
+    </div>
+  );
 }
